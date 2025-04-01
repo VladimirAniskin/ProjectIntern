b4588ab443060a89cefc960f2be93313935de719
Владимир <vova1210@yandex.ru>
2025-04-01 18:43:47 +0300

    Добавление ветки мастер в гитХаб

---
+++ src/main/java/com/intern/calorienormalizer/CalorieNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.intern.calorienormalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieNormalizerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CalorieNormalizerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/intern/calorienormalizer/CalorieNormalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.calorienormalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieNormalizerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
